9a697b85422f4dbb4f054afc38ae990ea97d03d7
Alexander Skorikov <alexander.skorikov@gmail.com>
2024-05-08 14:07:54 +0200

    Improve performance by using single precision GPU computations for G and H matrices

--- BEM/@bemret/private/initmat.m
+++ BEM/@bemret/private/initmat.m
@@ -1,9 +1,9 @@
 function obj = initmat( obj, enei )
 %  Initialize matrices for BEM solver.
-    
+
 %  use previously computed matrices ?
 if isempty( obj.enei ) || obj.enei ~= enei
-        
+
   obj.enei = enei;
 
   %%  auxiliary quantities
@@ -18,17 +18,24 @@ if isempty( obj.enei ) || obj.enei ~= enei
 
   %  simplify for unique dielectric functions
   if length( unique( diag( eps1 ) ) ) == 1 &&   ...
-     length( unique( diag( eps2 ) ) ) == 1 
+     length( unique( diag( eps2 ) ) ) == 1
     eps1 = full( eps1( 1 ) );
     eps2 = full( eps2( 1 ) );
   end
- 
+
   %%  Green functions and surface derivatives
-  G1 = obj.g{ 1, 1 }.G( enei ) - obj.g{ 2, 1 }.G( enei );  G1i = inv( G1 );
-  G2 = obj.g{ 2, 2 }.G( enei ) - obj.g{ 1, 2 }.G( enei );  G2i = inv( G2 );
+  G1 = obj.g{ 1, 1 }.G( enei ) - obj.g{ 2, 1 }.G( enei );
+  G2 = obj.g{ 2, 2 }.G( enei ) - obj.g{ 1, 2 }.G( enei );
+  G1 = gpuArray( single(G1) );
+  G2 = gpuArray( single(G2) );
 
   H1 = obj.g{ 1, 1 }.H1( enei ) - obj.g{ 2, 1 }.H1( enei );
   H2 = obj.g{ 2, 2 }.H2( enei ) - obj.g{ 1, 2 }.H2( enei );
+  H1 = gpuArray( single(H1) );
+  H2 = gpuArray( single(H2) );
+
+  G1i = inv( G1 );
+  G2i = inv( G2 );
 
   %%  L matrices [ Eq. (22) ]
   %    depending on the connectivity matrix, L1 and L2 can be
@@ -38,17 +45,17 @@ if isempty( obj.enei ) || obj.enei ~= enei
     L2 = eps2;
   else
     L1 = G1 * eps1 * G1i;
-    L2 = G2 * eps2 * G2i; 
+    L2 = G2 * eps2 * G2i;
   end
-  
+
   %%  Sigma and Delta matrices, and combinations
   %  Sigma matrices [ Eq.(21) ]
   Sigma1 = H1 * G1i;  clear G1 H1;
   Sigma2 = H2 * G2i;  clear G2 H2;
-   
+
   %  inverse Delta matrix
   Deltai = inv( Sigma1 - Sigma2 );
-   
+
   %  difference of dielectric functions
   L = L1 - L2;
   %  Sigma matrix
@@ -56,7 +63,7 @@ if isempty( obj.enei ) || obj.enei ~= enei
       k ^ 2 * ( ( L * Deltai ) .* ( nvec * nvec' ) ) * L;
 
   clear L Sigma2;
-  
+
   %%  save everything in class
 
   obj.k =  k;           %  wavenumber of light in vacuum
@@ -68,7 +75,7 @@ if isempty( obj.enei ) || obj.enei ~= enei
   obj.L1  = L1;                 %  G1 * eps1 * G1i, Eq. (22)
   obj.L2  = L2;                 %  G2 * eps2 * G2i
   obj.Sigma1 = Sigma1;          %  H1 * G1i, Eq. (21)
-  obj.Deltai = Deltai;          %  inv( Sigma1 - Sigma2 ) 
+  obj.Deltai = Deltai;          %  inv( Sigma1 - Sigma2 )
   obj.Sigmai = inv( Sigma );    %  Eq. (21,22)
 
 end
